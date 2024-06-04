0dcdf71ea617b4b6b64e7e0bfbc600f426efc583
Shubham Agarwal <66172189+shubham-s-agarwal@users.noreply.github.com>
2024-06-04 15:14:29 +0100

    Update test_llm_few.py

--- medcat/test_llm_few.py
+++ medcat/test_llm_few.py
@@ -40,9 +40,6 @@ from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain.prompts.few_shot import FewShotPromptTemplate
 
-
-examples = [{"text":"'care of Professor Riley at Queens Square as he had nerve conduction studies with them in order to rule out amyloid, he mentioned that his brother has amyloid realted neuropathy. He is also due to see Royal Free in about four weeks to discuss further options', 'amyloid'","label":"1"},{"text":"'Referred with possible history of recurrent pericarditis\n\n2. Family history of amyloidosis (uncle)\n\nMedication\n\nNil \n\nVisit Notes\n\nI had the pleasure of meeting Lee in the cardiac clinic today', 'amyloidosis'","label":"1"},{"text":"'High Ferritin 1512\nPlease see below for full set of bloods.\n \n \nJacob has been informed that legally he must not drive and must notify the DVLA of alcohol dependence. Licence will\nbe refused or revoked until a minimum of 1 year’s abstinence from alcohol consumption' , 'alcohol dependence'","label":"0"},{"text":"'diastolic dysfunction on echo).\n\n5. Seen by Prof Julian Gillmore at National Amyloidosis Centre, Royal Free Hospital: Fat biopsy consistent with TTR amyloidosis, genotype results awaited. SAP scan no visceral amyloid. DPD Jan 2021 strongly positive, Perugini 3' , 'TTR amyloidosis'","label":"0"},{"text":"'She has no chest pain, palpitations, coughing or wheezing. She does get shortness of breath, rashes, jaundice, bleeding or clotting disorders.','shortness of breath'","label":"2"},{"text":"'he felt slightly unwell just before seizure hence sat down\non the floor of the station, he is Unsure how long episode lasted. He had a previous seizure 20 days ago secondary to ETOH\nwithdrawal','seizure'","label":"2"}]
-
 prompt_template = '''
 Messages: {text}
 Categories: {label}
